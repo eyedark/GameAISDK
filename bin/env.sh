f93dffd820a56f15eb82bb98238ee282a139b385
Xuan Dien <xdienw@gmail.com>
2021-08-30 11:11:56 +0700

    fix openai bug

--- bin/env.sh
+++ bin/env.sh
@@ -1,5 +1,6 @@
 export AI_SDK_PROJECT_FULL_PATH=/home/xdien/workspace/GameAISDK/tools/SDKTool/project/LQ/
 export AI_SDK_PROJECT_FILE_PATH=/home/xdien/workspace/GameAISDK/tools/SDKTool/project/LQ/cfg/task/gameReg/Task.json
-
+ mount --rbind /mnt/docker_btrfs/data_train/trained-networks /home/xdien/workspace/GameAISDK/tools/SDKTool/project/LQ/data/trained-networks
+ mount --rbind /mnt/docker_btrfs/data_train/result/ /home/xdien/workspace/GameAISDK/tools/SDKTool/project/LQ/result/
 export AI_SDK_PROJECT_FULL_PATH=/home/xdien/workspace/GameAISDK/project/LQ3/
 export AI_SDK_PROJECT_FILE_PATH=/home/xdien/workspace/GameAISDK/project/LQ3/cfg/task/gameReg/Task.json 
