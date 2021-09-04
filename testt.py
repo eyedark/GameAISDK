880ee1aecd7c7a6470a8fb5f630ce75d9fc852cc
Xuan Dien <xdienw@gmail.com>
2021-09-04 10:04:55 +0700

    Clean the project, temporarily remove openai due to a lot of hardcode

---
+++ testt.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+def angle_between(p1, p2):
+    ang1 = np.arctan2(*p1[::-1])
+    ang2 = np.arctan2(*p2[::-1])
+    return np.rad2deg((ang1 - ang2) % (2 * np.pi))
+
+A = (50,70)
+B = (100,60)
+
+print(angle_between(A,B))
